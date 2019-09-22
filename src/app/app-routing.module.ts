04d4e47a91f08ea327471560b2d875c1333de3f7
Dip Parmar <parmar.dpp@gmail.com>
2019-09-22 22:11:13 +0530

    Configured Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'contacts', pathMatch: 'full' },
+  { path: 'contacts', component: ContactListComponent },
+  { path: 'contacts/create', component: ContactsComponent },
+  { path: 'contacts/:id', component: ContactDetailsComponent },
+  { path: 'contacts/:id/update', component: ContactsComponent },
+  { path: '**', redirectTo: 'contacts', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
